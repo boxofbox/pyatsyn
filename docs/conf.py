134c4915ed95ac3446e805f632cd6daa34fcd210
boxofbox <johnathanglyon@gmail.com>
2023-01-22 14:15:08 -0500

    readthedocs.io troubleshooting

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_mock_imports = ["numpy.random"]
+autodoc_mock_imports = ["ROOT","numpy.random"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
